2a3651939ccf118183a666c37c8a4e5f2639136b
Marcelo Lacerda <europio123@gmail.com>
2018-06-05 15:18:07 -0300

    Colocando salvamento em json.

--- jogo.py
+++ jogo.py
@@ -1,9 +1,12 @@
 import os
-import platform
+import json
 import getpass
+import platform
+
 
 maximo_jogadas = 10
 numero_cores = 7
+
 
 def limpar_tela():
 	if platform.system() == 'Windows':
@@ -11,9 +14,39 @@ def limpar_tela():
 	else:
 		os.system('clear')
 
-def definir_nova_senha():
+		
+def salvar_ranking(nome, pontuacao):
+	ranking = []
+	if os.path.isfile('ranking.json') == True:
+		f = open('ranking.json', 'r')
+		ranking_dict = json.load(f)
+		f.close()
+		for i in range(0, len(ranking_dict)):
+			ranking.append(ranking_dict[str(i)])
+	ranking_jogador = -1
+	if len(ranking) > 0:
+		for i in range(0, len(ranking)):
+			if pontuacao > ranking[i]['pontuacao']:
+				ranking.insert(i, {'nome': nome, 'pontuacao': pontuacao})
+				ranking_jogador = i
+				break
+	if ranking_jogador == -1:
+		ranking_jogador = len(ranking)
+		ranking.append({'nome': nome, 'pontuacao': pontuacao})
+	f = open('ranking.json', 'w')
+	ranking_json = {}
+	for i in range(0, len(ranking)):
+		ranking_json[str(i)] = ranking[i]
+	json.dump(ranking_json, f)
+	f.close()
+	print('Você obteve a ' + str(ranking_jogador + 1) + 'ª melhor pontuação!')
+
+	
+def vez_jogador_1():
+	limpar_tela()
 	senha = ''
-	if len(senha) != 4:
+	apenas_algarismos = False
+	while len(senha) != 4 or apenas_algarismos == False:
 		senha = getpass.getpass('Jogador 1, defina a senha com 4 algarismos de 0 a 6:')
 		if len(senha) != 4:
 			print('Sua senha deve possuir 4 algarismos.')
@@ -26,41 +59,62 @@ def definir_nova_senha():
 			if apenas_algarismos == False:
 				print('Sua senha deve conter apenas algarismos de 0 a 6')
 				input()
-			else:
-				limpar_tela()
-				acertou = False
-				for i in range(0, maximo_jogadas):
-					senha_adivinhada = input('Tentativa ' + str(i) + ': Jogador 2, tente adivinhar a senha:')
-					resposta = []
-					for j in range(0, len(senha)):
-						if senha_adivinhada[j] == senha[j]:
-							resposta.append('2')
-						elif senha_adivinhada[j] in senha:
-							resposta.append('1')
-						else:
-							resposta.append('0')
-					resposta_texto = ''
-					for char in resposta:
-						resposta_texto += char
-					print('Resposta do sistema: '+ resposta_texto)
-					if resposta_texto == '2222':
-						print('Parabéns! Você adivinhou a senha!')
-						acertou = True
-						break
-				if acertou == False:
-					print('Que pena... você não adivinhou a senha. A resposta correta é: ' + senha)
-				input('Pressione ENTER para voltar para o menu principal.')
-			
-def novo_jogo():
-	definir_nova_senha()
+	return senha
+
 	
+def vez_jogador_2(senha):
+	limpar_tela()
+	acertou = False
+	for i in range(0, maximo_jogadas):
+		senha_adivinhada = input('Tentativa ' + str(i) + ': Jogador 2, tente adivinhar a senha:')
+		resposta = []
+		for j in range(0, len(senha)):
+			if senha_adivinhada[j] == senha[j]:
+				resposta.append('2')
+			elif senha_adivinhada[j] in senha:
+				resposta.append('1')
+			else:
+				resposta.append('0')
+		resposta_texto = ''
+		for char in resposta:
+			resposta_texto += char
+		print('Resposta do sistema: '+ resposta_texto)
+		if resposta_texto == '2222':
+			print('Parabéns! Você adivinhou a senha!')
+			nome = input('Digite seu nome para o ranking.\n')
+			salvar_ranking(nome, maximo_jogadas - i)
+			acertou = True
+			break
+	if acertou == False:
+		print('Que pena... você não adivinhou a senha. A resposta correta é: ' + senha)
+
+
+def novo_jogo():
+	senha = vez_jogador_1()
+	vez_jogador_2(senha)
+	input('Pressione ENTER para voltar para o menu principal.')
+	
+
 def ver_ranking():
-	pass
+	limpar_tela()
+	if os.path.isfile('ranking.json') == True:
+		f = open('ranking.json', 'r')
+		ranking_dict = json.load(f)
+		f.close()
+		print('############ RANKING ############')
+		for i in range(0, len(ranking_dict)):
+			nome = ranking_dict[str(i)]['nome']
+			pontuacao = ranking_dict[str(i)]['pontuacao']
+			print(str(i + 1) + ' - ' + nome + ' - ' + str(pontuacao))
+	else:
+		print('Nenhuma pontuação foi registrada. Seja o primeiro a jogar!')
+	input()
+		
 
 opcao = 0
 while opcao != 3:
 	limpar_tela()
-	print('##########SENHA##########')
+	print('########## SENHA ##########')
 	print('1 - Novo Jogo')
 	print('2 - Ranking')
 	print('3 - Sair')
